0ebbeef7ea5f6b8c7c44414d60d92a28b60e9c9d
nathanos56 <nthnlwwlsn@gmail.com>
2024-04-14 15:58:08 -0600

    working on saving values

--- content_scripts/popup.js
+++ content_scripts/popup.js
@@ -1,6 +1,12 @@
+// switches
 const checkOffSwitch = document.getElementById('checkOffSwitch');
-const shortcutSwitch = document.getElementById('shortcutSwitch');
+// const shortcutSwitch = document.getElementById('shortcutSwitch');
 const customConfettiSwitch = document.getElementById('customConfettiSwitch');
+
+// buttons
+const applyButton = document.getElementById('applyButton');
+const testButton = document.getElementById('testButton');
+const resetButton = document.getElementById('resetButton');
 
 // Listen for switch toggle
 checkOffSwitch.addEventListener('change', (event) => {
@@ -9,7 +15,6 @@ checkOffSwitch.addEventListener('change', (event) => {
         console.log('Value is set to ' + event.target.checked);
     });
 });
-
 
 // real time range sliders
 window.onload = function() {
@@ -21,7 +26,30 @@ window.onload = function() {
         }
         slider.dispatchEvent(new Event('input'));
     });
+    
+    // show switch states
+    var switches = document.querySelectorAll('[role="switch"]');
+    switches.forEach(function(mySwitch) {
+        chrome.storage.sync.get(mySwitch.id, function(result) {
+            mySwitch.checked = result[mySwitch.id] ? true : false;
+        });
+    });
 }
+
+// Save the state of all of the switches and sliders in storage
+applyButton.addEventListener('click', (event) => {
+    // switches
+    var switches = document.querySelectorAll('[role="switch"]');
+    switches.forEach(function(mySwitch) {
+        chrome.storage.sync.set(mySwitch.id, mySwitch.checked);
+    });
+    // sliders
+    var sliders = document.querySelectorAll('.form-range');
+    sliders.forEach(function(slider) {
+        chrome.storage.sync.set(slider.id, document.getElementById(slider.id + 'Value').textContent);
+    });
+});
+
 
 // apply button
 // chrome.storage.sync.set({label: this.value}, function() {
